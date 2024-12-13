456901e9cdaf6b58af9a0200a06f968a4d2966bf
sanonesan <ezcudze@gmail.com>
2024-12-13 10:55:58 +0300

    change train_bandits.py according to bandits.py changes

--- src/bandits/train_bandit.py
+++ src/bandits/train_bandit.py
@@ -5,11 +5,13 @@ Script to train and save the hierarchical bandit-based recommendation system.
 from src.bandits.bandit import Recommender, RecommenderConfig
 
 if __name__ == "__main__":
-    config = RecommenderConfig(top_k=1, reward_interactions=30)
     recommender = Recommender(
-        items_data_path="./data/items.csv",
-        interactions_data_path="./data/interactions.csv",
-        predicted_categories_path="./data/predicted_categories.csv",
-        config=config,
+        config=RecommenderConfig(
+            top_k=1,
+            reward_interactions=30,
+            items_data_path="./data/items.csv",
+            interactions_data_path="./data/interactions.csv",
+            predicted_categories_path="./data/predicted_categories.csv",
+        ),
     )
     recommender.fit()
